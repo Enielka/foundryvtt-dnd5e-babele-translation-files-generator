ce3cfc821e077192f5d45cc0eb887d6f1e2bd679
Alexis KLEINE <alexy_du02@live.fr>
2025-01-17 23:32:48 +0100

    Ignore conditions effects

--- scripts/exporters/actor-exporter.mjs
+++ scripts/exporters/actor-exporter.mjs
@@ -20,8 +20,13 @@ export class ActorExporter extends AbstractExporter {
     }
 
     if (AbstractExporter._hasContent(document.effects)) {
-      documentData.effects = {};
-      document.effects.filter(effect => !effect._tombstone).forEach(effect => {
+      const conditionsToIgnore = [
+        "dnd5eblinded0000", "dnd5eexhaustion0", "dnd5eincapacitat", "dnd5epetrified00", "dnd5erestrained0",
+        "dnd5estunned0000", "dnd5epoisoned000", "dnd5einvisible00", "dnd5efrightened0", "dnd5echarmed0000",
+        "dnd5edeafened000", "dnd5egrappled000", "dnd5eparalyzed00", "dnd5eprone000000", "dnd5eunconscious"
+      ];
+      document.effects.filter(effect => !conditionsToIgnore.includes(effect._id) && !effect._tombstone).forEach(effect => {
+        documentData.effects = documentData.effects ?? {};
         const { _id, name, description, changes } = effect;
         const changesObj = (changes && Array.isArray(changes)) ? changes.reduce((acc, change) => {
           if (change.key === 'name') acc.name = change.value;
@@ -30,7 +35,7 @@ export class ActorExporter extends AbstractExporter {
         }, {}) : {};
 
         const effectData = { name, ...description && { description }, ...Object.keys(changesObj).length && { changes: changesObj } };
-        
+
         const key = documentData.effects[name] && !foundry.utils.objectsEqual(documentData.effects[name], effectData) ? _id : name;
         documentData.effects[key] = effectData;
       });
@@ -42,16 +47,16 @@ export class ActorExporter extends AbstractExporter {
   async _processDataset() {
     const documents = await this.pack.getIndex();
 
-    for (const indexDocument of documents) {     
+    for (const indexDocument of documents) {
       const documentData = ActorExporter.getDocumentData(
         await this.pack.getDocument(indexDocument._id),
         this.options.customMapping,
         true
       );
-      
+
       let key = this.options.useIdAsKey ? indexDocument._id : indexDocument.name;
       key = this.dataset.entries[key] && !foundry.utils.objectsEqual(this.dataset.entries[key], documentData) ? indexDocument._id : key;
-      
+
       this.dataset.entries[key] = foundry.utils.mergeObject(documentData, this.existingContent[key] ?? {});
 
       this._stepProgressBar();
